d4a58ae2a7c54893ccbc2c09201ab817933bf066
Toshiaki Baba <toshiaki@netmark.jp>
2012-03-09 01:01:09 +0900

    initial import

---
+++ ror3.2_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ror32Example::Application.config.secret_token = '011791ad01f6b80b17c788d7ba6fa94077a44748b0de79a62251ddeeeee95bd1e594d7fd1bb6894cb6bfda55ca60e57a057fc9d3d0ad710ff47831432e6806d0'
